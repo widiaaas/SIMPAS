9c27041352e8ac527131c1591dcc280a2523e31d
abigail <abigailhutapea13@gmail.com>
2025-01-17 11:10:01 +0700

    sesuaiin sm layout

--- resources/views/mentor/detail.blade.php
+++ resources/views/mentor/detail.blade.php
@@ -1,108 +1,72 @@
-<html>
-    <head>
-    <title>Detail Peserta - SIMPAS </title>
-     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" rel="stylesheet"/>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Aoboshi+One&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+@extends('layouts.app')
 
-        .inter-font {
-            font-family: "Inter", serif;
-            font-optical-sizing: auto;
-            font-weight: 400;
-            font-style: normal;
-        }
-        .aoboshi-one-regular {
-            font-family: "Aoboshi One", serif;
-            font-weight: 400;
-            font-style: normal;
-        }
-    </style>
-    </head>
-<body class="bg-[#fdf7f4] inter-font">
-    <div class="flex h-screen">
-    <!-- Sidebar -->
-    <x-sidebar-mentor></x-sidebar-mentor>
-    <!--main content-->
-    <div class="w-screen p-8 rounded-tl-[30px] rounded-bl-[30px] bg-[#fdf7f4] overflow-auto">
-        <div class="mb-8">
-            <a class="text-[#282A4C] text-lg mb-4 block inter-font font-bold" href="/mentor/daftarPeserta">
-                <i class="fas fa-arrow-left">
-                </i>
-                Kembali
-            </a>
-        </div>
-        <div class="flex justify-between items-center">
-            <h2 class="text-2xl aoboshi-one-regular font-bold text-[#b12a2f]">
-               Detail Peserta Magang
-            </h2>
-            <div class="flex items-center justify-end">
-                <div class="text-right">
-                    <p class="aoboshi-one-regular text-sm text-[#b12a2f]">Pemerintah Kota</p>
-                    <p class="aoboshi-one-regular text-sm text-[#b12a2f]">Semarang</p>
-                </div>
-                <img src="{{asset('img/pemkot.png')}}" class="ml-2" width="30" alt="Logo Pemerintah Kota Semarang" />
-                </div>
-            </div>
-        <div class="mt-2 bg-[#FEF7F4] rounded-lg p-14">
-            <div class="space-y-4">
-            <div class="flex space-x-4 justify-between">
-              <div>
-                <p class="text-sm font-semibold text-gray-600">Nama:</p>
-                <p class="text-lg font-medium">Widiawati Sihaloho</p>
-              </div>
-              <div >
-                <p class="text-sm font-semibold text-gray-600">Detail CV:</p>
-                <button class=" px-9 py-2 bg-[#282a4c] text-white rounded-lg hover:bg-blue-600 transition">
-                  Lihat CV
-                </button>
-              </div>
-            </div>
-            <div class="flex space-x-4 justify-between">
-                <div>
-                    <p class="text-sm font-semibold text-gray-600">Sekolah/Universitas Asal:</p>
-                    <p class="text-lg font-medium">Universitas Diponegoro</p>
-                  </div>
-                <div >
-                  <p class="text-sm font-semibold text-gray-600">Detail Proposal:</p>
-                  <button class=" px-4 py-2 bg-[#282a4c] text-white rounded-lg hover:bg-blue-600 transition">
-                    Lihat Proposal
-                  </button>
-                </div>
-              </div>
-              <div>
-                <p class="text-sm font-semibold text-gray-600">NIM/NISN:</p>
-                <p class="text-lg font-medium">24060122130037</p>
-              </div>
-              <div>
-                <p class="text-sm font-semibold text-gray-600">Jurusan:</p>
-                <p class="text-lg font-medium">Informatika</p>
-              </div>
-              <div>
-                <p class="text-sm font-semibold text-gray-600">Email:</p>
-                <p class="text-lg font-medium">widiawatiscollege@gmail.com</p>
-              </div>
-              <div>
-                <p class="text-sm font-semibold text-gray-600">Diterima Magang di:</p>
-                <p class="text-lg font-medium">Dinas Komunikasi, Informasi, Statistik, dan Persandian Kota Semarang</p>
-              </div>
-              <div>
-                <p class="text-sm font-semibold text-gray-600">Bidang:</p>
-                <p class="text-lg font-medium">Statistik</p>
-              </div>
-              <div class="flex space-x-4">
-                <div>
-                  <p class="text-sm font-semibold text-gray-600">Tanggal Mulai:</p>
-                  <p class="text-lg font-medium">02/01/2025</p>
-                </div>
-                <div>
-                  <p class="text-sm font-semibold text-gray-600">Tanggal Selesai:</p>
-                  <p class="text-lg font-medium">12/02/2025</p>
-                </div>
-              </div>
-            </div>
-        </div>
+@section('title', 'Profil Peserta - SIMPAS')
+
+@section('content')
+<div class="mb-8">
+  <a class="text-[#282A4C] text-lg mb-4 block inter-font font-bold" href="/mentor/daftarPeserta">
+      <i class="fas fa-arrow-left">
+      </i>
+      Kembali
+  </a>
+</div>
+<h1 class="header">Profil Peserta Magang</h1>        
+<div class="mt-1 bg-[#FDF2EE] rounded-lg p-12 inter-font">
+  <div class="space-y-4">
+  <div class="flex space-x-4 justify-between">
+    <div>
+      <p class="text-sm font-semibold text-gray-600">Nama:</p>
+      <p class="text-lg font-medium">Widiawati Sihaloho</p>
     </div>
-</body>
+    <div >
+      <p class="text-sm font-semibold text-gray-600">Detail CV:</p>
+      <button class=" px-9 py-2 bg-[#282a4c] text-white rounded-lg hover:bg-blue-600 transition">
+        Lihat CV
+      </button>
+    </div>
+  </div>
+  <div class="flex space-x-4 justify-between">
+      <div>
+          <p class="text-sm font-semibold text-gray-600">Sekolah/Universitas Asal:</p>
+          <p class="text-lg font-medium">Universitas Diponegoro</p>
+        </div>
+      <div >
+        <p class="text-sm font-semibold text-gray-600">Detail Proposal:</p>
+        <button class=" px-4 py-2 bg-[#282a4c] text-white rounded-lg hover:bg-blue-600 transition">
+          Lihat Proposal
+        </button>
+      </div>
+    </div>
+    <div>
+      <p class="text-sm font-semibold text-gray-600">NIM/NISN:</p>
+      <p class="text-lg font-medium">24060122130037</p>
+    </div>
+    <div>
+      <p class="text-sm font-semibold text-gray-600">Jurusan:</p>
+      <p class="text-lg font-medium">Informatika</p>
+    </div>
+    <div>
+      <p class="text-sm font-semibold text-gray-600">Email:</p>
+      <p class="text-lg font-medium">widiawatiscollege@gmail.com</p>
+    </div>
+    <div>
+      <p class="text-sm font-semibold text-gray-600">Diterima Magang di:</p>
+      <p class="text-lg font-medium">Dinas Komunikasi, Informasi, Statistik, dan Persandian Kota Semarang</p>
+    </div>
+    <div>
+      <p class="text-sm font-semibold text-gray-600">Bidang:</p>
+      <p class="text-lg font-medium">Statistik</p>
+    </div>
+    <div class="flex space-x-4">
+      <div>
+        <p class="text-sm font-semibold text-gray-600">Tanggal Mulai:</p>
+        <p class="text-lg font-medium">02/01/2025</p>
+      </div>
+      <div>
+        <p class="text-sm font-semibold text-gray-600">Tanggal Selesai:</p>
+        <p class="text-lg font-medium">12/02/2025</p>
+      </div>
+    </div>
+
+
+@endsection 
--- resources/views/mentor/profil.blade.php
+++ resources/views/mentor/profil.blade.php
@@ -1,130 +1,90 @@
-<html>
-    <head>
-    <title>Profil Mentor - SIMPAS </title>
-     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" rel="stylesheet"/>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Aoboshi+One&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+@extends('layouts.app')
 
-        .inter-font {
-            font-family: "Inter", serif;
-            font-optical-sizing: auto;
-            font-weight: 400;
-            font-style: normal;
-        }
-        .aoboshi-one-regular {
-            font-family: "Aoboshi One", serif;
-            font-weight: 400;
-            font-style: normal;
-        }
-    </style>
-    </head>
-<body class="bg-[#fdf7f4] font-roboto">
-    <div class="flex h-screen">
-    <!-- Sidebar -->
-    <x-sidebar-mentor></x-sidebar-mentor>
-    <!-- Main Content -->
-    <div class="flex-1 p-8">
-        <!-- Header Profil -->
-        <div class="mb-10">
-            <div class="flex justify-between items-center">
-                <h2 class="text-2xl aoboshi-one-regular font-bold text-[#b12a2f]">
-                    Profil
+@section('title', 'Profil Mentor - SIMPAS')
+
+@section('content')
+<h1 class="header">Profil</h1>        
+   <!-- Main Content -->
+   <div class="flex-1 p-8">
+    <!-- Kotak Profil -->
+    <div class="p-8 rounded-lg shadow-md bg-white">
+        <div class="flex items-center mb-6">
+            <div>
+                <h2 class="aoboshi-one-regular text-2xl font-bold text-[#3e2c2c]">
+                    Arif Budiman, S.Kom.
                 </h2>
-                <div class="flex items-center">
-                    <div class="text-right">
-                        <p class="aoboshi-one-regular text-sm text-[#b12a2f]">Pemerintah Kota</p>
-                        <p class="aoboshi-one-regular text-sm text-[#b12a2f]">Semarang</p>
-                    </div>
-                    <img src="{{asset('img/pemkot.png')}}" class="ml-2" width="30" alt="Logo Pemerintah Kota Semarang" />
-                </div>
+                <p class="aoboshi-one-regular text-lg text-[#3e2c2c]">
+                    NIP. 2344578400009
+                </p>
             </div>
         </div>
-    
-        <!-- Kotak Profil -->
-        <div class="p-8 rounded-lg shadow-md bg-white">
-            <div class="flex items-center mb-6">
-                <div>
-                    <h2 class="aoboshi-one-regular text-2xl font-bold text-[#3e2c2c]">
-                        Arif Budiman, S.Kom.
-                    </h2>
-                    <p class="aoboshi-one-regular text-lg text-[#3e2c2c]">
-                        NIP. 2344578400009
-                    </p>
-                </div>
+        <hr class="border-t border-gray-300 mb-6" />
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div>
+                <label class="inter-font italic block text-sm font-medium text-gray-700">
+                    NIP
+                </label>
+                <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2 inter-font">
+                    2344578400009
+                </p>
             </div>
-            <hr class="border-t border-gray-300 mb-6" />
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div>
-                    <label class="inter-font italic block text-sm font-medium text-gray-700">
-                        NIP
-                    </label>
-                    <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2">
-                        2344578400009
-                    </p>
-                </div>
-                <div>
-                    <label class=" inter-font italic block text-sm font-medium text-gray-700">
-                        Nomor Telepon
-                    </label>
-                    <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2">
-                        087832630688
-                    </p>
-                </div>
-                <div>
-                    <label class="inter-font italic block text-sm font-medium text-gray-700">
-                        Nama
-                    </label>
-                    <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2">
-                        Arif Budiman, S.Kom.
-                    </p>
-                </div>
-                <div>
-                    <label class="inter-font italic block text-sm font-medium text-gray-700">
-                        Email
-                    </label>
-                    <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2">
-                        arifbudiman@gmail.com
-                    </p>
-                </div>
-                <div>
-                    <label class="inter-font italic block text-sm font-medium text-gray-700">
-                        Instansi
-                    </label>
-                    <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2">
-                        Dinas Komunikasi, Informatika, Statistik, dan Persandian
-                    </p>
-                </div>
-                <div>
-                    <label class="inter-font italic block text-sm font-medium text-gray-700">
-                        Alamat
-                    </label>
-                    <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2">
-                        Jalan Kedondong no. 9, Kedungmundu, Semarang.
-                    </p>
-                </div>
-                <div>
-                    <label class="inter-font italic block text-sm font-medium text-gray-700">
-                        Jabatan
-                    </label>
-                    <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2">
-                        Kepala Bidang Sistem Pemerintahan Berbasis Elektronik
-                    </p>
-                </div>
+            <div>
+                <label class=" inter-font italic block text-sm font-medium text-gray-700">
+                    Nomor Telepon
+                </label>
+                <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2 inter-font">
+                    087832630688
+                </p>
             </div>
-            <div class="mt-6 text-right">
-                <a class="aoboshi-one-regular bg-[#ff8a65] text-white rounded-lg hover:font-bold hover:bg-orange-500 px-6 py-2" href="/mentor/editProfil">
-                    Edit
-                </a>
+            <div>
+                <label class="inter-font italic block text-sm font-medium text-gray-700">
+                    Nama
+                </label>
+                <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2 inter-font">
+                    Arif Budiman, S.Kom.
+                </p>
             </div>
+            <div>
+                <label class="inter-font italic block text-sm font-medium text-gray-700">
+                    Email
+                </label>
+                <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2 inter-font">
+                    arifbudiman@gmail.com
+                </p>
+            </div>
+            <div>
+                <label class="inter-font italic block text-sm font-medium text-gray-700">
+                    Instansi
+                </label>
+                <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2 inter-font">
+                    Dinas Komunikasi, Informatika, Statistik, dan Persandian
+                </p>
+            </div>
+            <div>
+                <label class="inter-font italic block text-sm font-medium text-gray-700">
+                    Alamat
+                </label>
+                <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2 inter-font">
+                    Jalan Kedondong no. 9, Kedungmundu, Semarang.
+                </p>
+            </div>
+            <div>
+                <label class="inter-font italic block text-sm font-medium text-gray-700">
+                    Jabatan
+                </label>
+                <p class="bg-[#ffccbc] text-[#3e2c2c] rounded-lg px-4 py-2 inter-font">
+                    Kepala Bidang Sistem Pemerintahan Berbasis Elektronik
+                </p>
+            </div>
+        </div>
+        <div class="mt-6 text-right">
+            <a class="aoboshi-one-regular bg-[#ff8a65] text-white rounded-lg hover:font-bold hover:bg-orange-500 px-6 py-2" href="/mentor/editProfil">
+                Edit
+            </a>
         </div>
     </div>
-    
-   </div>
-  </div>
- </body>
-</html>
+</div>
 
+</div>
 
+@endsection 
