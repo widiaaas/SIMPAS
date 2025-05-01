45a7735acd47b180f288e86bedd4bb3d16a76067
abigail <abigailhutapea13@gmail.com>
2025-05-01 13:30:05 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call([
+            UserSeeder::class,
+            InstansiSeeder::class,
+            KoordinatorSeeder::class,
+            MentorSeeder::class,
+            PesertaMagangSeeder::class,
+            PendaftaranMagangSeeder::class,
+            PenilaianSeeder::class,
         ]);
     }
 }
